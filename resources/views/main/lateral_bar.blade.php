8265568665552d8bc7cd91a29e17b9e9f906fd2b
unknown <gabikochem55@gmail.com>
2024-09-12 17:57:38 -0300

    Tratamento de erros | Alteracao das corres da sidebar

--- resources/views/main/lateral_bar.blade.php
+++ resources/views/main/lateral_bar.blade.php
@@ -4,8 +4,8 @@
 </head>
 
 <aside class="sidebar">
-    <a href="">Pessoas</a>
-    <a href="/produto">Produtos</a>
-    <a href="/pdv">PDV Mobile</a>
+    <a href="#" class="pessoas"><img src="https://lodi.hom.sgmaster.com.br/icons/people.svg" class="img_pessoas">Pessoas</a>
+    <a href="/produto" class="produto"><img class="img_produto" src="http://lodi.hom.sgmaster.com.br/icons/stock.svg">Produtos</a>
+    <a href="/pdv" class="pdv"><img src="https://lodi.hom.sgmaster.com.br/icons/PDVMobile.svg" class="img_pdv">PDV Mobile</a>
 
 </aside>
