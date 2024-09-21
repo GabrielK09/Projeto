328c618fe0aa2c6c17f0d3fff3a5cd217141b321
TsukiKtk <87103106+TsukiKtk@users.noreply.github.com>
2024-09-20 22:30:52 -0300

    Ajustes

--- resources/views/pessoas/pessoas.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="scope">
-    @include('main.sidebar')
-   
-    <h1>Todas as pessoas</h1>
-
-    <a href="/pessoas/cadastro"><button class="btn cadastrarPessoas">Cadastrar </button></a>
-
-</div>
